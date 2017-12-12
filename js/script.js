18876099f20d0ddc0ca7364e9a4714f04671f3c8
Roslyn Harris <rharris0113@gmail.com>
2017-12-11 23:40:50 -0800

    added click() listener in jquery to enter button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 $('#number').text('Whoop there it is!'); });
 
+$(document).ready(function(){
+    $("#enter").click(function(){
+      $("#number").text("Whoop there it is!");
+  });
+ });
+
 // window.onload = function() {
 	// document.getElementById("number").innerHTML = "Counting now bitch!"

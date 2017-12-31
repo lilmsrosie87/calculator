54d21883b6515405408de791ac28864cc715287e
Roslyn Harris <rharris0113@gmail.com>
2017-12-31 00:32:39 -0800

    adding jQuery for number length and functions for clicking buttions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
-
 $(document).ready(function(){
-    $("#enter").click(function(){
-      $("#number").text("Whoop there it is!");
-  });
+	var testNumLength = function(number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length-9,9));
+            if (number.length > 15) {
+                number = "";
+                totaldiv.text("Err");
+            }
+        } 
+    };
+	var number = ""
+	var newnumber = ""
+	var operator = ""
+	var string = ""
+	var totaldiv = $(#total)
+	total.text("0")
+	
 });
+
+
+// $(document).ready(function(){
+    // $("#enter").click(function(){
+      // $("#number").text("Whoop there it is!");
+  // });
+// });
 
 // window.onload = function() {
 	// document.getElementById("number").innerHTML = "Counting now bitch!"
